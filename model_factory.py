693275d83ea3793a7c5fc6ae95403af7affc3d44
Matheus Gorski <mgorski@eng.ucsd.edu>
2021-02-17 15:34:30 -0800

    "added code"

---
+++ model_factory.py
@@ -0,0 +1,15 @@
+################################################################################
+# CSE 253: Programming Assignment 4
+# Code snippet by Ajit Kumar, Savyasachi
+# Fall 2020
+################################################################################
+
+# Build and return the model here based on the configuration.
+def get_model(config_data, vocab):
+    hidden_size = config_data['model']['hidden_size']
+    embedding_size = config_data['model']['embedding_size']
+    model_type = config_data['model']['model_type']
+
+    # You may add more parameters if you want
+
+    raise NotImplementedError("Model Factory Not Implemented")
